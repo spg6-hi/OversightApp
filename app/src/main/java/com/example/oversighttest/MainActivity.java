f7e3294d8e53fc20595262a0181a3e9fdcc6cfe0
spg6-hi <spg6@hi.is>
2022-02-16 21:56:17 +0000

    basic layout edit

--- app/src/main/java/com/example/oversighttest/MainActivity.java
+++ app/src/main/java/com/example/oversighttest/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.oversighttest;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import com.example.overesighttest.databinding.ActivityMainBinding;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+
+    //private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(R.layout.activity_main);
     }
 }
