7083797352ff51727f4733cd58c73a1e06be0fae
krisak11 <kristoferisak@gmail.com>
2022-02-21 16:47:54 +0000

    Bætti við nýjum Activity skrám og xml

---
+++ app/src/main/java/com/example/oversighttest/TransactionActivity.java
@@ -0,0 +1,19 @@
+package com.example.oversighttest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TransactionActivity extends AppCompatActivity {
+    //private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
