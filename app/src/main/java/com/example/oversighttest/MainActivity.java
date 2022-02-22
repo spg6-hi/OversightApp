9a23153fca1eba330526ef847b3e8de594c7840a
spg6-hi <spg6@hi.is>
2022-02-22 17:56:58 +0000

    bætti við tab layout með fragments og fragment manager

--- app/src/main/java/com/example/oversighttest/MainActivity.java
+++ app/src/main/java/com/example/oversighttest/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.oversighttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 //import com.example.overesighttest.databinding.ActivityMainBinding;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     //private ActivityMainBinding binding;
 
@@ -17,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
         //binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabs);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        PageAdapter tabManager = new PageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tabManager.addFragment(new TransactionsPage(), "Transactions");
+        tabManager.addFragment(new SpendingPlanPage(), "Spending Plan");
+        tabManager.addFragment(new BankPage(), "Bank");
+        viewPager.setAdapter(tabManager);
     }
 }
