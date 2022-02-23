6c485844aaf70e13681d92d9596cba825b7ed046
spg6-hi <spg6@hi.is>
2022-02-23 02:19:02 +0000

    Bætti við pie chart í transaction fragmentið

--- app/src/main/java/com/example/oversighttest/TransactionsPage.java
+++ app/src/main/java/com/example/oversighttest/TransactionsPage.java
@@ -1,19 +1,93 @@
 package com.example.oversighttest;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TransactionsPage extends Fragment {
+
+    private PieChart pieChart;
+    private View v;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_transactions_page, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        v =  getView();
+        pieChart = (PieChart) v.findViewById(R.id.pieChart);
+        setupPieChart();
+        loadPieChartData();
+    }
+
+    private void setupPieChart(){
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText("Spending by Category");
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setRotationEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+
+    }
+
+    private void  loadPieChartData(){
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.1f, "Food"));
+        entries.add(new PieEntry(0.3f, "Home"));
+        entries.add(new PieEntry(0.2f, "Entertainment"));
+        entries.add(new PieEntry(0.5f, "Car repairs"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS){
+            colors.add(color);
+        }
+        for (int color: ColorTemplate.VORDIPLOM_COLORS){
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+
     }
 }
