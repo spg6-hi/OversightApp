6de09733e3016be6eb6b16230698ae2fafe41bd4
spg6-hi <spg6@hi.is>
2022-03-13 14:59:05 +0000

    ...

--- app/src/main/java/com/example/oversighttest/pages/SpendingPlanPage.java
+++ app/src/main/java/com/example/oversighttest/pages/SpendingPlanPage.java
@@ -2,6 +2,8 @@ package com.example.oversighttest.pages;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,5 +20,12 @@ public class SpendingPlanPage extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_spending_plan_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
