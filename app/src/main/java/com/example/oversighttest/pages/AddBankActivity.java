d78ced601b38fef45ad476b446c794a035efefff
krisak11 <81173550+krisak11@users.noreply.github.com>
2022-04-19 16:53:33 +0000

    linechart komið fyrir bank balance

--- app/src/main/java/com/example/oversighttest/pages/AddBankActivity.java
+++ app/src/main/java/com/example/oversighttest/pages/AddBankActivity.java
@@ -2,10 +2,10 @@ package com.example.oversighttest.pages;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -13,12 +13,22 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.oversighttest.R;
+import com.example.oversighttest.entities.Category;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -27,8 +37,8 @@ public class AddBankActivity extends AppCompatActivity {
     private EditText mFundsAdded;
     private Button confirmButton;
     private Button cancelButton;
-    private LineChart mLineChart;
-    private RadioGroup mPeriodRadioGroup;
+
+
 
 
     @Override
@@ -36,33 +46,7 @@ public class AddBankActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bank);
         mFundsAdded = (EditText) findViewById(R.id.mFundsRemoved);
-        //Linechart that displays bank balance over time
-        mLineChart = findViewById(R.id.activity_add_bank_linechart);
-        //Radio Buttons to select time interval for the line chart (1 day, 30 days,...)
-        mPeriodRadioGroup = (RadioGroup) findViewById(R.id.activity_add_bank_period_radiogroup);
-        mPeriodRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // checkedId is the RadioButton selected
-                View radioButton = mPeriodRadioGroup.findViewById(checkedId);
-                int index = mPeriodRadioGroup.indexOfChild(radioButton);
 
-                // Add logic here
-
-                switch (index) {
-                    case 0: // first button
-
-                        Toast.makeText(getApplicationContext(), "Selected button number " + index, Toast.LENGTH_LONG).show();
-                        break;
-                    case 1: // secondbutton
-
-                        Toast.makeText(getApplicationContext(), "Selected button number " + index, Toast.LENGTH_LONG).show();
-                        break;
-                }
-            }
-        });
-        configureLineChart();
         /*
         Confirm button
         When pressed, get value from text field and return it
@@ -106,23 +90,6 @@ public class AddBankActivity extends AppCompatActivity {
         });
     }
 
-    private void configureLineChart() {
-        Description desc = new Description();
-        desc.setText(String.valueOf(R.string.mLineChart));
-        desc.setTextSize(28);
-        mLineChart.setDescription(desc);
-
-        XAxis xAxis = mLineChart.getXAxis();
-        xAxis.setValueFormatter(new ValueFormatter() {
-            private final SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM", Locale.ENGLISH);
-
-            @Override
-            public String getFormattedValue(float value) {
-                long millis = (long) value * 1000L;
-                return mFormat.format(new Date(millis));
-            }
-        });
-    }
 
     public static Intent newIntent(Context packageContext){
         Intent i = new Intent(packageContext, AddBankActivity.class);
